953289a60e2ece0eb1ddb2cc16270d6a49a49cfc
Miguel Araya Cortes <miguel.araya02@alumnos.ucn.cl>
2025-09-28 19:17:54 -0300

    feat(init): added empty models and partial information, also swagger for future documentation of functions

--- backend-app/routes/api.php
+++ backend-app/routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/process', [ProcessController::class, 'process']);
+Route::get('/process', [ProcessController::class, 'index']);
+Route::get('/process/{id}', [ProcessController::class, 'show']);
+Route::put('/process/{id}', [ProcessController::class, 'update']);
+Route::delete('/process/{id}', [ProcessController::class, 'destroy']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
