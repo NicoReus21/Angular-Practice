e0d831506bea09e77736332859b2084e6b15801b
NicoReus21 <nicolas.carmona01@alumnos.ucn.cl>
2025-10-01 16:55:33 -0300

    feat(routes): add news routes to api.php

--- backend-app/routes/api.php
+++ backend-app/routes/api.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\ProcessController;
 use App\Http\Controllers\ReporteFlashController;
 use App\Http\Controllers\DiabController;
 use App\Http\Controllers\ObacController;
-use App\Http\Controllers\ReporteController;
 use App\Http\Controllers\CertificadoCarabineroController;
 use App\Http\Controllers\InformeMedicoController;
 use App\Http\Controllers\OtroDocumentoMedicoController;
@@ -26,46 +25,49 @@ use App\Http\Controllers\CertificadoMedicoAtencionEspecialController;
 use App\Http\Controllers\CertificadoMedicoTrasladoController;
 use App\Http\Controllers\BoletaGastoAcompananteController;
 use App\Http\Controllers\OtroGastoController;
+use App\Http\Controllers\CertificadoMedicoIncapacidadController;
+use App\Http\Controllers\DeclaracionTestigoController;
+use App\Http\Controllers\DauController;
+use App\Http\Controllers\DocumentController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/process', [ProcessController::class, 'process']);
-Route::get('/process', [ProcessController::class, 'index']);
-Route::get('/process/{id}', [ProcessController::class, 'show']);
-Route::put('/process/{id}', [ProcessController::class, 'update']);
-Route::delete('/process/{id}', [ProcessController::class, 'destroy']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::patch('/processes/{process}/complete-step', [ProcessController::class, 'completeStep']);
+// Rutas para el CRUD
+Route::post('/process', [ProcessController::class, 'store']);
+Route::get('/process', [ProcessController::class, 'index']);  
+Route::get('/process/{process}', [ProcessController::class, 'show']); 
+Route::put('/process/{process}', [ProcessController::class, 'update']);     
+Route::delete('/process/{process}', [ProcessController::class, 'destroy']);  
+Route::delete('/documents/{document}', [DocumentController::class, 'destroy']);
 // Requerimiento Operativo
-Route::post('process/{id}/upload_reporte_flash', [ReporteFlashController::class, 'upload'])->name('upload.reporte_flash');
-Route::post('process/{id}/upload_diab', [DiabController::class, 'upload'])->name('upload.diab');
-Route::post('process/{id}/upload_obac', [ObacController::class, 'upload'])->name('upload.obac');
-//TODO: cambiar Controlador al correcto
-Route::post('process/{id}/upload_incidente_sin_lesiones', [ReporteController::class, 'upload.reporte_flash']);
+Route::post('process/{id}/upload_reporte_flash', [ReporteFlashController::class, 'store']);
+Route::post('process/{id}/upload_diab', [DiabController::class, 'store']);
+Route::post('process/{id}/upload_obac', [ObacController::class, 'store']);
+Route::post('process/{id}/upload_copia_libro_guardia', [CopiaLibroGuardiaController::class, 'store']);
+Route::post('process/{id}/upload_declaracion_testigo', [DeclaracionTestigoController::class, 'store']);
 //Antecedentes Generales
-Route::post('process/{id}/upload_certificado_carabineros', [CertificadoCarabineroController::class, 'upload'])->name('upload.certificado_carabineros');
-Route::post('process/{id}/upload_informe_medico', [InformeMedicoController::class, 'upload'])->name('upload.informe_medico');
-Route::post('process/{id}/upload_otros_documento_medico_adicional', [OtroDocumentoMedicoController::class, 'upload'])->name('upload.otros_documento_medico_adicional');
+Route::post('process/{id}/upload_certificado_carabineros', [CertificadoCarabineroController::class, 'store']);
+Route::post('process/{id}/upload_dau', [DauController::class, 'store']);
+Route::post('process/{id}/upload_informe_medico', [InformeMedicoController::class, 'store']);
+Route::post('process/{id}/upload_otros_documento_medico_adicional', [OtroDocumentoMedicoController::class, 'store']);
+Route::post('process/{id}/upload_certificado_medico_atencion_especial', [CertificadoMedicoAtencionEspecialController::class, 'store']);
 //Documento del Cuerpo de Bomberos
-Route::post('process/{id}/upload_certificado_acreditacion_voluntario', [CertificadoAcreditacionVoluntarioController::class, 'upload'])->name('upload.certificado_acreditacion_voluntario')  ;
-Route::post('process/{id}/upload_copia_libro_guardia', [CopiaLibroGuardiaController::class, 'upload'])->name('upload.copia_libro_guardia');
-Route::post('process/{id}/upload_copia_libro_llamada', [CopiaLibroLlamadaController::class, 'upload'])->name('upload.copia_libro_llamada');
-Route::post('process/{id}/upload_aviso_citacion', [AvisoCitacionController::class, 'upload'])->name('upload.aviso_citacion');
-Route::post('process/{id}/upload_copia_lista_asistencia', [CopiaListaAsistenciaController::class, 'upload'])->name('upload.copia_lista_asistencia');
-Route::post('process/{id}/upload_informe_ejecutivo', [InformeEjecutivoController::class, 'upload'])->name('upload.informe_ejecutivo');
+Route::post('process/{id}/upload_certificado_acreditacion_voluntario', [CertificadoAcreditacionVoluntarioController::class, 'store']);
+Route::post('process/{id}/upload_copia_libro_llamada', [CopiaLibroLlamadaController::class, 'store']);
+Route::post('process/{id}/upload_aviso_citacion', [AvisoCitacionController::class, 'store']);
+Route::post('process/{id}/upload_copia_lista_asistencia', [CopiaListaAsistenciaController::class, 'store']);
+Route::post('process/{id}/upload_informe_ejecutivo', [InformeEjecutivoController::class, 'store']);
 //Prestaciones Medica
-Route::post('process/{id}/upload_factura_prestaciones', [FacturaPrestacionController::class, 'upload'])->name('upload.factura_prestaciones');
-Route::post('process/{id}/upload_boleta_honorarios_visada', [BoletaHonorarioVisadaController::class, 'upload'])->name('upload.boleta_honorarios_visada');
-Route::post('process/{id}/upload_boleta_medicamentos', [BoletaMedicamentoController::class, 'upload'])->name('upload.boleta_medicamentos');
-Route::post('process/{id}/upload_certificado_medico_autorizacion_examen', [CertificadoMedicoAutorizacionExamenController::class, 'upload'])->name('upload.certificado_medico_autorizacion_examen');
+Route::post('process/{id}/upload_factura_prestaciones', [FacturaPrestacionController::class, 'store']);
+Route::post('process/{id}/upload_boleta_honorarios_visada', [BoletaHonorarioVisadaController::class, 'store']);
+Route::post('process/{id}/upload_boleta_medicamentos', [BoletaMedicamentoController::class, 'store']);
+Route::post('process/{id}/upload_certificado_medico_autorizacion_examen', [CertificadoMedicoAutorizacionExamenController::class, 'store']);
 //Gastos de Traslados y alimentacion
-Route::post('process/{id}/upload_boleta_factura_traslado', [BoletaFacturaController::class, 'upload'])->name('upload.boleta_factura_traslado');
-Route::post('process/{id}/upload_certificado_medico_atencion_especial', [CertificadoMedicoAtencionEspecialController::class, 'upload'])->name('upload.certificado_medico_atencion_especial');
-Route::post('process/{id}/upload_certificado_medico_traslado', [CertificadoMedicoTrasladoController::class, 'upload'])->name('upload.certificado_medico_traslado');
-Route::post('process/{id}/upload_boleta_gastos_acompanante', [BoletaGastoAcompananteController::class, 'upload'])->name('upload.boleta_gastos_acompanante');
-//TODO: cambiar controlador al correcto
-Route::post('process/{id}/upload_boleta_alimentacion_acompanante', [ProcessController::class, 'upload.reporte_flash']);
-Route::post('process/{id}/upload_gastos_otros', [OtroGastoController::class, 'upload.reporte_flash']);
+Route::post('process/{id}/upload_boleta_factura_traslado', [BoletaFacturaController::class, 'store']);
+Route::post('process/{id}/upload_certificado_medico_traslado', [CertificadoMedicoTrasladoController::class, 'store']);
+Route::post('process/{id}/upload_boleta_gastos_acompanante', [BoletaGastoAcompananteController::class, 'store']);
+Route::post('process/{id}/upload_certificado_medico_incapacidad', [CertificadoMedicoIncapacidadController::class, 'store']);
+Route::post('process/{id}/upload_boleta_alimentacion_acompanante', [BoletaGastoAcompananteController::class, 'store']);
+Route::post('process/{id}/upload_otros_gastos', [OtroGastoController::class, 'store']);
