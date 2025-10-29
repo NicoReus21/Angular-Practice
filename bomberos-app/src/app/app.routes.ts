b58f5b0e55d2dcc22671391a0214d21f37ab6ae0
NicoReus21 <nicolas.carmona01@alumnos.ucn.cl>
2025-10-29 16:09:26 -0300

    feat: added new paths to routes.

--- bomberos-app/src/app/app.routes.ts
+++ bomberos-app/src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { HistorialComponent } from './components/historial/historial';
 import { DocumentUploadComponent } from './components/document-upload/document-upload';
+import { MachineHistorialComponent } from './components/machine-historial/machine-historial';
+import { CreateFiretruckComponent } from './components/create-firetruck/create-firetruck';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -9,4 +12,6 @@ export const routes: Routes = [
   { path: 'historial', component: HistorialComponent },
   { path: 'document-upload', component: DocumentUploadComponent },
   { path: 'document-upload/:id', component: DocumentUploadComponent },
+  { path: 'machine-historial', component: MachineHistorialComponent },
+  { path: 'create-firetruck', component: CreateFiretruckComponent }, // <-- AÑADIDO
 ];
