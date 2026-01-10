0b3e6aa386f5baac0c6cb4a85fb72b240cf5ac26
Miguel Araya Cortes <miguel.araya02@alumnos.ucn.cl>
2026-01-10 14:01:09 -0300

    fix: added google client id

--- bomberos-app/src/environments/environment.prod.ts
+++ bomberos-app/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://sigba-api.itmanagement.cl/api',
   backendUrl: 'https://sigba-api.itmanagement.cl',
-  googleClientId: ''
+  googleClientId: '345099488682-690pg0agcgtc8hin8og4bvclf48k4l9s.apps.googleusercontent.com'
 };
