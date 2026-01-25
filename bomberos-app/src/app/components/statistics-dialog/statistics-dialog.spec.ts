84d22c3f35628e612686a02954b92be46421aca1
NicoReus21 <nicolas.carmona01@alumnos.ucn.cl>
2026-01-25 17:01:17 -0300

    Test: Correct the error in the statistics-dialog.

--- bomberos-app/src/app/components/statistics-dialog/statistics-dialog.spec.ts
+++ bomberos-app/src/app/components/statistics-dialog/statistics-dialog.spec.ts
@@ -1,22 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StatisticsDialogComponent } from './statistics-dialog';
+import { MachineHistorialService } from '../../services/machine-historial';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { of } from 'rxjs';
 
-describe('StatisticsDialog', () => {
+describe('StatisticsDialogComponent', () => {
   let component: StatisticsDialogComponent;
   let fixture: ComponentFixture<StatisticsDialogComponent>;
+
+  const mockService = {
+    getAnnualBudget: jasmine.createSpy('getAnnualBudget').and.returnValue(of({ year: 2025, amount: 100000 })),
+    saveAnnualBudget: jasmine.createSpy('saveAnnualBudget').and.returnValue(of({ year: 2025, amount: 200000 }))
+  };
+
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
+
+  const mockDialogData = {
+    units: [] 
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StatisticsDialogComponent],
+      imports: [StatisticsDialogComponent], 
+      providers: [
+        { provide: MachineHistorialService, useValue: mockService },
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: mockDialogData }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(StatisticsDialogComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); 
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getAnnualBudget on init', () => {
+    expect(mockService.getAnnualBudget).toHaveBeenCalled();
   });
 });
